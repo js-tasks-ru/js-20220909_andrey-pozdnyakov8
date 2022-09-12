91d7feb3120dc6ca31a0f77ef3e1ed40b4edc00c
unknown <exekuta@gmail.com>
2022-09-12 17:57:07 +0300

    fix: change task 2-1 solution

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -9,15 +9,27 @@ export function sortStrings(arr, param) {
 
   switch (param) {
   case 'asc':
-    sortedArr.sort((a, b) => a.localeCompare(b, 'kf', { caseFirst: 'upper' }));
+    sortedArr.sort((a, b) => a.localeCompare(b, 'ru', { caseFirst: 'upper' }));
     break;
   case 'desc':
-    sortedArr.sort((a, b) => b.localeCompare(a, 'kf', { caseFirst: 'upper' }));
+    sortedArr.sort((a, b) => b.localeCompare(a, 'ru', { caseFirst: 'upper' }));
     break;
   default:
-    sortedArr.sort((a, b) => a.localeCompare(b, 'kf', { caseFirst: 'upper' }));
+    sortedArr.sort((a, b) => a.localeCompare(b, 'ru', { caseFirst: 'upper' }));
     break;
   }
 
   return sortedArr;
 }
+
+// const data = [
+//   'Соска (пустышка) NUK 10729357',
+//   'ТВ тюнер D-COLOR  DC1301HD',
+//   'Детский велосипед Lexus Trike Racer Trike',
+//   'Соска (пустышка) Philips SCF182/12',
+//   'Powerbank аккумулятор Hiper SP20000'
+// ];
+// sortStrings(data, asc);
+// sortStrings(data, desc);
+
+// {"description":"should correctly sort strings for mixed \"en\" and \"ru\" locales","success":false,"suite":["javascript-data-types/sort-strings"],"time":4}
