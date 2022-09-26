0b4631c3a458a0b2d5cec83c3a635d6560678f0e
unknown <exekuta@gmail.com>
2022-09-26 22:47:58 +0300

    feat: add tasks 5

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,56 @@
 export default class NotificationMessage {
+  constructor(message = "", { duration = 0, type = "" } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
 
+    this.render();
+  }
+
+  get notificationTemplate() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  show() {
+    if (this.element) {this.remove();}
+
+    document.body.append(this.element);
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+
+    wrapper.innerHTML = this.notificationTemplate;
+    // console.log(wrapper.innerHTML);
+    // console.log(wrapper.outerHTML);
+    this.element = wrapper.firstElementChild;
+    // NotificationMessage.element = wrapper.firstElementChild;
+    
+  }
+
+  remove() {
+    if (this.element) {this.element.remove();}
+    clearTimeout(this.element.timeout);
+  }
+
+  destroy() {
+    this.element.remove();
+  }
 }
+
+const btn1 = document.querySelector("#btn1");
+
+
+
+
